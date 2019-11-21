87d1f09bee5d05921ddf165643054062a42207bc
Mike <mike.ashby93@gmail.com>
2019-11-20 21:34:06 -0500

    Blank styled home page

---
+++ src/main/java/com/visionary/visionary/VisionaryApplication.java
@@ -0,0 +1,13 @@
+package com.visionary.visionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionaryApplication.class, args);
+	}
+
+}
