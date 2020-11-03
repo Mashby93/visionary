d6e7f4bbb47e67a49d8830771e65ebf5afefbaf7
Mike <mike.ashby93@gmail.com>
2020-11-02 22:10:35 -0500

    Add event description

--- src/main/java/com/visionary/visionary/domain/Event.java
+++ src/main/java/com/visionary/visionary/domain/Event.java
@@ -10,6 +10,21 @@ import java.util.List;
 @Table(name = "event")
 public class Event extends AbstractEntity {
     private List<DateRange> dates;
+    private String description;
 
+    public List<DateRange> getDates() {
+        return dates;
+    }
 
+    public void setDates(List<DateRange> dates) {
+        this.dates = dates;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
