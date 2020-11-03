48688b8191260f27d42288947dbc94494c34ee12
Mike <mike.ashby93@gmail.com>
2020-11-02 22:04:14 -0500

    Build date range object

--- src/main/java/com/visionary/visionary/model/DateRange.java
+++ src/main/java/com/visionary/visionary/model/DateRange.java
@@ -1,4 +1,24 @@
 package com.visionary.visionary.model;
 
+import java.util.Date;
+
 public class DateRange {
+    private Date startTime;
+    private Date endTime;
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 }
